97bca8c4f4e0aa8ed5550e173f452275c39b5a3b
z61379as <alexandru.staicu@student.manchester.ac.uk>
2020-02-29 15:08:15 +0000

    printed the locations of all the ireland atms

--- chatbot.py
+++ chatbot.py
@@ -2,9 +2,19 @@ import json
 import requests
 with open ('api/ireland_atm.json') as f:
     atm_data = json.load(f)
-print(atm_data["meta"])
- 
-# printing location
+lat = float(atm_data["data"]["Brand"][0]["ATM"][0]["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Latitude"])
+long = float(atm_data["data"]["Brand"][0]["ATM"][0]["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Longitude"])
+
+#print (lat,long)
+#print(float(atm_data["data"]["Brand"][0]["ATM"][3]["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Latitude"]))
+
+
+for atm in atm_data["data"]["Brand"][0]["ATM"]:
+    lat_atm = atm["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Latitude"]
+    long_atm = atm["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Longitude"]
+    print(lat_atm,long_atm)
+
+# printing current location
 res = requests.get("https://ipinfo.io/")
 loc_data = res.json()
 print(loc_data["loc"])
