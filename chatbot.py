e053bdb701ee5ef4b89a734e94abf6d206136f94
z61379as <alexandru.staicu@student.manchester.ac.uk>
2020-02-29 14:44:08 +0000

    managed to get the location of the user

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,10 @@
 import json
+import requests
 with open ('api/ireland_atm.json') as f:
     atm_data = json.load(f)
 print(atm_data["meta"])
+ 
+# printing location
+res = requests.get("https://ipinfo.io/")
+loc_data = res.json()
+print(loc_data["loc"])
