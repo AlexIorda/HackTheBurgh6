21d0af46a813ea1934c5faefa3ceef878c1a9f74
z61379as <alexandru.staicu@student.manchester.ac.uk>
2020-03-01 02:57:32 +0000

    closest distance to atm and best bank depending on questions

--- chatbot.py
+++ chatbot.py
@@ -12,21 +12,6 @@ current_lat = float(current_loc[0])
 current_long = float(current_loc[1])
 
 
-def closest_distance_to_atm(s):
-    #returns the closest atm for bank with the path s (exp api/ireland_atm.json)
-    #daca are in json dupa data [] trb scoase de la inceput si final
-    with open (s) as f:
-        atm_data = json.load(f)    
-    mn = 10**100
-    for atm in atm_data["data"]["Brand"][0]["ATM"]:
-        lat_atm = float(atm["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Latitude"])
-        long_atm = float(atm["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Longitude"])
-        #print(lat_atm,long_atm)
-        dist = math.sqrt( (lat_atm - current_lat) ** 2 + (long_atm - current_long) ** 2)
-        print(dist)
-        if (dist < mn):
-            mn = dist
-    return mn
 def get_minimum_balance_repayment_rate(s): # cat platesti inapoi pe luna minim
     #returns the minimum balance ...  for bank with the path s
     #daca are in json dupa data [] trb scoase de la inceput si final
@@ -64,16 +49,17 @@ def print_benefits(s):#beneficii dubioase
         pca_data = json.load(f)
     for elem in pca_data["data"]["Brand"][0]["PCA"][0]["PCAMarketingState"][0]["FeaturesAndBenefits"]["FeatureBenefitItem"]:
         print(elem["Name"])
-        
-        
-        
+   
+data_dict_Q1 = {}
+data_dict_Q2 = {}
+data_dict_Q3 = {}
+data_dict_Q4 = {}     
+ 
+    
 def make_dict_points():
+    global data_dict_Q1, data_dict_Q2, data_dict_Q3, data_dict_Q4 
     with open ("api/sondaj_varste.json") as f:
         data = json.load(f)
-    data_dict_Q1 = {}
-    data_dict_Q2 = {}
-    data_dict_Q3 = {}
-    data_dict_Q4 = {}
     answers1 = {"Extremely likely" : 6, "Very likely": 5, "Fairly likely": 4, "Unlikely": 3,
                "NOT USED IN RANKING: Don't know": 2, "NOT USED IN RANKING: Do not recommend": 1}
     answers2 = {"Extremely likely" : 7, "Very likely": 6, "Fairly likely": 5, "Unlikely": 4,
@@ -93,7 +79,15 @@ def make_dict_points():
                        "Lloyds Bank" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "NatWest" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "Royal Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
-                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
+                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Clydesdale Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "first direct": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Metro Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Nationwide": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Tesco Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "The Co-operative Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "TSB": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Yorkshire Bank":  {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
                     
                     }
     data_dict_Q2 = { "Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
@@ -104,8 +98,15 @@ def make_dict_points():
                        "Lloyds Bank" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "NatWest" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "Royal Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
-                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
-                    
+                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Clydesdale Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "first direct": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Metro Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Nationwide": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Tesco Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "The Co-operative Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "TSB": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Yorkshire Bank":  {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
                     }
     data_dict_Q3 = { "Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                       "Royal Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
@@ -115,7 +116,15 @@ def make_dict_points():
                        "Lloyds Bank" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "NatWest" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "Royal Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
-                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
+                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Clydesdale Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "first direct": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Metro Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Nationwide": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Tesco Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "The Co-operative Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "TSB": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Yorkshire Bank":  {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
                     
                     }
     data_dict_Q4 = { "Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
@@ -126,20 +135,74 @@ def make_dict_points():
                        "Lloyds Bank" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "NatWest" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
                        "Royal Bank of Scotland" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
-                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
-                    
+                       "Santander" : {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Clydesdale Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "first direct": {0: "16-24", "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Metro Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Nationwide": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Tesco Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "The Co-operative Bank": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "TSB": {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0},
+                       "Yorkshire Bank":  {"16-24" : 0, "25-34" : 0, "35-44": 0, "45-54": 0, "55-64": 0, "65+": 0}
                     }
-    for sample in data["Data"]["Brand"][0]["Data"]:
-       # print(sample)
-        if "PCAQ1All" in sample:
-            data_dict_Q1[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers1[sample["PCAQ1All"]]
-        if "PCAQ2All" in sample:
-            data_dict_Q2[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers2[sample["PCAQ2All"]]
-        if "PCAQ3All" in sample:
-            data_dict_Q3[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers3[sample["PCAQ3All"]]
-        if "PCAQ4All" in sample:
-            data_dict_Q4[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers4[sample["PCAQ4All"]]
+    for sample1 in data["Data"]["Brand"]:
+        for sample in sample1["Data"]:
+            print(sample)
+            if "PCAQ1All" in sample:
+                data_dict_Q1[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers1[sample["PCAQ1All"]]
+                if "PCAQ2All" in sample:
+                    data_dict_Q2[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers2[sample["PCAQ2All"]]
+                    if "PCAQ3All" in sample: 
+                        data_dict_Q3[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers3[sample["PCAQ3All"]]
+                        if "PCAQ4All" in sample:
+                            data_dict_Q4[sample["Brand"]][sample["Age"]] += sample["Weight"] *  answers4[sample["PCAQ4All"]]
     print(data_dict_Q1["Bank of Scotland"]["25-34"])
+def best_bank(dictionary, age):
+    banks = ["Bank of Scotland", "Royal Bank of Scotland", "Barclays", "Halifax", "HSBC UK",  "Lloyds Bank", "NatWest", "Royal Bank of Scotland",
+             "Santander", "Clydesdale Bank", "first direct", "Metro Bank", "Nationwide", "Tesco Bank", "The Co-operative Bank", "TSB", "Yorkshire Bank"]
+    mx = 0
+    mxbank = ""
+    for bank in banks:
+        print(dictionary[bank])
+        if (dictionary[bank][age_segment(age)] >= mx):
+            mx = dictionary[bank][age_segment(age)]
+            mxbank = bank
+    return mxbank
+def age_segment(age):
+    if (age <= 24):
+        return "16-24"
+    elif (age <= 34):
+        return "25-34"
+    elif (age <= 44):
+        return "35-44"
+    elif (age <= 54):
+        return "45-54"
+    elif (age <= 64):
+        return "55-64"
+    else:
+        return "65+"
+
+path_list = ["api/atm/barclays_atm.json", "api/atm/firsttrustbank_atm.json", "api/atm/ireland_atm.json",
+             "api/atm/natwest_atm.json", "api/atm/scotland_atm.json", "api/atm/ulster_atm.json", "api/atm/lloyds_atm.json",
+             "api/atm/royal_scotland_atm.json", "api/atm/danske_atm.json", "api/atm/first_trust_atm.json", "api/atm/halifax_atm.json",
+             "api/atm/hsbc_atm.json", "api/atm/nbs_atm.json", "api/atm/santander_atm.json"]
+def closest_distance_to_atm(s):
+    MN = 10**101
+    for bank in s:
+        with open (bank) as f:
+            atm_data = json.load(f)    
+        mn = 10**100
+        for atm in atm_data["data"]["Brand"][0]["ATM"]:
+            lat_atm = float(atm["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Latitude"])
+            long_atm = float(atm["Location"]["PostalAddress"]["GeoLocation"]["GeographicCoordinates"]["Longitude"])
+            #print(lat_atm,long_atm)
+            dist = math.sqrt( (lat_atm - current_lat) ** 2 + (long_atm - current_long) ** 2)    
+            #print(dist)
+            if (dist < mn):
+                mn = dist
+        if mn < MN:
+            MN = mn
+    return MN
 #print(min(closest_distance_to_atm("api/ireland_atm.json"), closest_distance_to_atm("api/natwest_atm.json")))
 #print_benefits("api/natwest_pca.json")
 list_minimum_credit_limit = [get_minimum_credit_limit("api/ireland_ccc.json"), get_minimum_credit_limit("api/natwest_ccc.json")]
@@ -150,6 +213,22 @@ print("I will help you choose the right bank for you!")
 print("But firrrrst....enter some information in order to help me")
 age = int(input("How old are you?"))
 income = float(input("What is your income/month?"))
+print("Now I'm gonna get the perfect bank for youuuuuuuuu")
+print("I want to get to know you better. What do you prefer:")
+print("[1] The perfect bank in general")
+print("[2] The best branch services")
+print("[3] The best mobile banking services")
+print("[4] The best overdraft services")
+choice = int(input("Enter choice please:"))
+if (choice == 1):
+    print(best_bank(data_dict_Q1, age))
+elif (choice == 2):
+    print(best_bank(data_dict_Q2, age))
+elif (choice == 3):
+    print(best_bank(data_dict_Q3, age))
+elif (choice == 4):
+    print(best_bank(data_dict_Q4, age))
+print(closest_distance_to_atm(path_list))
 for val in list_minimum_credit_limit:
     if (val > income/3):
         list_minimum_credit_limit.remove(val)
